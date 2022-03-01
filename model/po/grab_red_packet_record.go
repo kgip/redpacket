6b513f7c857553fc3e31c7cac251ec5ace634f8d
Xiangguang.li <3275637019@qq.com>
2022-03-01 12:43:22 +0800

    新增本地消息队列

--- model/po/grab_red_packet_record.go
+++ model/po/grab_red_packet_record.go
@@ -5,7 +5,7 @@ var GrabRedPacketRecordModel = &GrabRedPacketRecord{}
 // GrabRedPacketRecord 用户抢红包记录
 type GrabRedPacketRecord struct {
 	Base
-	UserId      uint    `gorm:"type:int(10);not null;index:user_redpacket_idx"`
-	RedPacketId uint    `gorm:"type:int(10);not null;index:user_redpacket_idx"`
+	RedPacketId uint    `gorm:"type:int(10);not null;index:redpacket_user_idx"`
+	UserId      uint    `gorm:"type:int(10);not null;index:redpacket_user_idx"`
 	Amount      float64 `gorm:"not null;default 0"`
 }
