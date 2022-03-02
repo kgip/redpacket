46e012381234d35e6f2cb0c3f09dc69f7da13300
Xiangguang.li <3275637019@qq.com>
2022-03-02 17:37:58 +0800

    添加抢红包接口

--- utils/constant/redis.go
+++ utils/constant/redis.go
@@ -2,8 +2,6 @@ package constant
 
 const (
 	RedPacketKeyPrefix            = "{redpacket}:"
-	RedPacketHSetCountField       = "count"
-	RedPacketHSetBalanceField     = "balance"
 	GrabRedPacketUserSetKeyPrefix = RedPacketKeyPrefix + "users:"
 	RedPacketLockKeyPrefix        = "redpacket:"
 )
